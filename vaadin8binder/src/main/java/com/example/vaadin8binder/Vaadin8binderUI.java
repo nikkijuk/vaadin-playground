943f16aa5cf4b68850a3bae97ef2dd2b4207bc88
nikkijuk <jukka.nikki@kolumbus.fi>
2017-12-22 15:36:39 +0100

    catched validation exception to be shown in ui

--- vaadin8binder/src/main/java/com/example/vaadin8binder/Vaadin8binderUI.java
+++ vaadin8binder/src/main/java/com/example/vaadin8binder/Vaadin8binderUI.java
@@ -1,11 +1,16 @@
 package com.example.vaadin8binder;
 
 import com.vaadin.data.Binder;
+import com.vaadin.data.BinderValidationStatus;
 import com.vaadin.data.ValidationException;
+import com.vaadin.data.ValidationResult;
 import com.vaadin.data.converter.StringToBigDecimalConverter;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.ui.*;
 import com.vaadin.annotations.Theme;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Theme("Vaadin8binder")
 public class Vaadin8binderUI extends UI{
@@ -21,8 +26,9 @@ public class Vaadin8binderUI extends UI{
     private Label debug = new Label ("Debug");
 
     private Button doLogin = new Button ("Login");
+    private Button doValidate = new Button ("Validate");
 
-	// binder tied to pojo
+    // binder tied to pojo
 	private Binder<Login> binder = new Binder<>(Login.class);
 
 	@Override
@@ -36,10 +42,10 @@ public class Vaadin8binderUI extends UI{
 		// define email binding, is value is not given it's empty
 		binder.forMemberField(email).withNullRepresentation("");
 
+		// define secret code, null presentation, and converter
         binder.forMemberField(secret)
                 .withNullRepresentation("")
                 .withConverter(new StringToBigDecimalConverter("Conversion error"));
-
 
         // close bindings which are done with forMemberField
 		binder.bindInstanceFields(this);
@@ -53,6 +59,7 @@ public class Vaadin8binderUI extends UI{
 		// add binder level validator
 		binder.withValidator(l -> checkUserIdentity(l), "Username or email needs to be given" );
 
+		// set label for bean level error messages
         binder.setStatusLabel(messages);
 
         // create login bean
@@ -63,23 +70,47 @@ public class Vaadin8binderUI extends UI{
 
         // writes value back to login pojo
         doLogin.addClickListener(e -> login(login));
+        doValidate.addClickListener(e -> validateBinder());
 
         VerticalLayout layout = new VerticalLayout();
-		layout.addComponents(username, email, password, secret, register, messages, doLogin, debug);
-
+		layout.addComponents(username, email, password, secret, register, messages, debug, doLogin, doValidate);
 		setContent(layout);
 	}
+
+    private void validateBinder() {
+        try {
+            if (binder.hasChanges()) {
+                // error happens here
+                // see: https://github.com/vaadin/framework/issues/9955
+                BinderValidationStatus<Login> validationStatus = binder.validate();
+                List<ValidationResult> validationErrors = validationStatus.getValidationErrors(); // both bean & field errors
+                boolean valid = validationErrors.isEmpty();
+                if (!valid) {
+                    String msg = validationErrors
+                            .stream()
+                            .map(error -> error.getErrorMessage())
+                            .collect(Collectors.joining(", "));
+                    debug.setValue(msg);
+                } else {
+                    debug.setValue("no binder validation errors");
+                }
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+            debug.setValue(e.getMessage());
+        }
+    }
 
     private void login(Login l) {
         try {
             if (binder.hasChanges()) {
-                binder.writeBean(l);
+                binder.writeBean(l); // write buffered changes back to bean
                 debug.setValue("values written back to bean");
             } else {
                 debug.setValue("no changes at binder");
             }
         } catch (ValidationException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
             debug.setValue(e.getMessage());
         }
     }
